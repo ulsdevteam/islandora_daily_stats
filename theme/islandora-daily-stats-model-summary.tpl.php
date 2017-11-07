30e0985f9ef24a25015e80675c98e1616a16929b
Brian Gillingham <bgilling@pitt.edu>
2017-11-07 21:50:55 +0000

    updated summary to pull real data

--- theme/islandora-daily-stats-model-summary.tpl.php
+++ theme/islandora-daily-stats-model-summary.tpl.php
@@ -1,6 +1,6 @@
   <div class="model_summary">
     <h3><?php print $name; ?></h3>
-    <div class="value cell_numeric"><?php print number_format($yesterday); ?></div><label>Objects yesterday</label><br>
+    <div class="value cell_numeric"><?php print number_format($today); ?></div><label>Objects today</label><br>
     <div class="value cell_numeric"><?php print number_format($last_7_days); ?></div><label>Last 7 days</label><br>
     <div class="value cell_numeric"><?php print number_format($prev_7_days); ?></div><label>Prev 7 days</label><br>
     <div class="value cell_numeric"><?php print number_format($last_30_days); ?></div><label>Last 30 days</label><br>
