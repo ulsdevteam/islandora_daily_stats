06695e64cf5b7b59bbb99269c024d4fa2ee9b500
Brian Gillingham <bgilling@pitt.edu>
2017-11-08 15:23:25 +0000

    changed overview summary to compare a day values to a previous day as new objects in last week, last two weeks, last month, etc

--- theme/islandora-daily-stats-model-summary.tpl.php
+++ theme/islandora-daily-stats-model-summary.tpl.php
@@ -1,8 +1,8 @@
   <div class="model_summary">
-    <h3><?php print $name; ?></h3>
-    <div class="value cell_numeric"><?php print number_format($today); ?></div><label>Objects today</label><br>
-    <div class="value cell_numeric"><?php print number_format($last_7_days); ?></div><label>Last 7 days</label><br>
-    <div class="value cell_numeric"><?php print number_format($prev_7_days); ?></div><label>Prev 7 days</label><br>
-    <div class="value cell_numeric"><?php print number_format($last_30_days); ?></div><label>Last 30 days</label><br>
-    <div class="value cell_numeric"><?php print number_format($prev_30_days); ?></div><label>Prev 30 days</label><br>
+      <h3><?php print $name; ?> - <span class="today_value"><?php print number_format($total_today); ?> total objects</span></h3>
+    <div class="value cell_numeric"><?php print number_format($new_today); ?></div><label>New today</label><br>
+    <div class="value cell_numeric"><?php print number_format($new_in_last_week); ?></div><label>Added in last week</label><br>
+    <div class="value cell_numeric"><?php print number_format($new_in_two_weeks); ?></div><label>Added in last two weeks</label><br>
+    <div class="value cell_numeric"><?php print number_format($new_in_last_month); ?></div><label>Added in last 30 days</label><br>
+    <div class="value cell_numeric"><?php print number_format($new_in_two_months); ?></div><label>Added in last 60 days</label><br>
   </div>
