3a48c0f9946fee5a2aa52f75387e6bb65a283e50
Willow Gillingham <bgilling@pitt.edu>
2019-02-08 16:09:40 -0500

    made the summary page display a tiny bit more simpler to read

--- theme/islandora-daily-stats-model-summary.tpl.php
+++ theme/islandora-daily-stats-model-summary.tpl.php
@@ -1,15 +1,21 @@
   <div class="model_summary">
       <h3><?php print $name; ?> - <span class="today_value"><?php print number_format($total_today); ?> total objects</span></h3>
-    <div class="value cell_numeric"><?php print number_format($new_today); ?></div><label>New today</label>
+    <label>today</label>
+      <span class="value cell_numeric<?php print ($new_today < 0 ? " negative" : ""); ?>"><?php print ($new_today < 0  ? '(' : '') . number_format(abs($new_today)) . ($new_today < 0 ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($yesterday_count); ?></span><br>
-    <div class="value cell_numeric"><?php print number_format($new_in_last_week); ?></div><label>Added in last week</label>
+    <label>last week</label>
+      <span class="value cell_numeric<?php print ($new_in_last_week < 0 ? " negative" : ""); ?>"><?php print ($new_in_last_week < 0  ? '(' : '') . number_format(abs($new_in_last_week)) . ($new_in_last_week < 0  ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($last_7_count); ?></span><br>
-    <div class="value cell_numeric"><?php print number_format($new_in_two_weeks); ?></div><label>Added in last two weeks</label>
+    <label>last two weeks</label>
+      <span class="value cell_numeric<?php print ($new_in_two_weeks < 0 ? " negative" : ""); ?>"><?php print ($new_in_two_weeks < 0  ? '(' : '') . number_format(abs($new_in_two_weeks)) . ($new_in_two_weeks < 0  ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($prev_7_days); ?></span><br>
-    <div class="value cell_numeric"><?php print number_format($new_in_last_month); ?></div><label>Added in last 30 days</label>
+    <label>last 30 days</label>
+      <span class="value cell_numeric<?php print ($new_in_last_month < 0 ? " negative" : ""); ?>"><?php print ($new_in_last_month < 0  ? '(' : '') . number_format(abs($new_in_last_month)) . ($new_in_last_month < 0  ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($last_30_count); ?></span><br>
-    <div class="value cell_numeric"><?php print number_format($new_in_two_months); ?></div><label>Added in last 60 days</label>
+    <label>last 60 days</label>
+      <span class="value cell_numeric<?php print ($new_in_two_months < 0 ? " negative" : ""); ?>"><?php print ($new_in_two_months < 0  ? '(' : '') . number_format(abs($new_in_two_months)) . ($new_in_two_months < 0  ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($prev_30_days); ?></span><br>
-    <div class="value cell_numeric"><?php print number_format($new_last_year); ?></div><label>Added in last year (365 days)</label>
+    <label>last year (365 days)</label>
+      <span class="value cell_numeric<?php print ($new_last_year < 0 ? " negative" : ""); ?>"><?php print ($new_last_year < 0  ? '(' : '') . number_format(abs($new_last_year)) . ($new_last_year < 0  ? ')' : ''); ?></span>
       <span class="cell_numeric sub_value"><?php print number_format($prev_365_days); ?></span><br>
   </div>
